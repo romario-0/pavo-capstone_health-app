a5dbbd7178b88b63124ff5fc8428fcebd1a80164
VishalPhirkoj <vishalphirkoj13@gmail.com>
2022-04-14 20:34:04 +0530

    user registration done

---
+++ capstone-backend/controllers/user.controller.js
@@ -0,0 +1,37 @@
+const User = require("../models/user.model");
+const dotenv = require("dotenv");
+dotenv.config("../.env");
+
+const getUser = async (req, res) => {
+  try {
+    let tokenId = getToken(req.headers.authorization);
+    const getUserDetail = await User.findById({ _id: tokenId }).select(
+      "-password"
+    );
+    res.json({ userDetail: getUserDetail });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addUser = async (req, res) => {
+  try {
+    const checkEmail = await User.findOne({ email: req.body.email }); // checking email is allreday taken or not
+    if (checkEmail) {
+      res.json({ message: "Email is already Registerd.." });
+    } else {
+      const data = new User(req.body);
+      const response = await data.save();
+      res.json({
+        message: "Registration Done",
+        _id: response._id,
+        fullName: response.fullname,
+        email: response.email,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getUser, addUser };
--- capstone-backend/index.js
+++ capstone-backend/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 4000 || process.env.PORT;
 require("./db/conn");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", require("./routes/user.route"));
 
 app.get("/", (req, res) => {
   res.send(`Pavo backend `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Sever is running on${PORT}`);
+  console.log(`Sever is running on Port ${PORT}`);
 });
