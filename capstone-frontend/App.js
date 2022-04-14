c84c130ec182103bb2ba509d550c910d276c7f47
Prasanth Siripurapu <prasanthpspk01@gmail.com>
2022-04-14 20:05:34 +0530

    SignuUp Desihn

--- capstone-frontend/App.js
+++ capstone-frontend/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open!</Text>
+      <Signup/> 
       <StatusBar style="auto" /> 
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    backgroundColor: '#36485f',
+    paddingLeft: 60,
+    paddingRight: 60,
   },
 });
