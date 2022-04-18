295f9a78d455a81efbf64afd14e3e22365883ecc
VishalPhirkoj <vishalphirkoj13@gmail.com>
2022-04-18 22:27:59 +0530

    some changes

--- capstone-backend/index.js
+++ capstone-backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 require("./db/conn");
 const cors = require("cors");
 const dotenv = require("dotenv");
