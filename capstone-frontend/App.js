d4831913d455295a1a0f3f3ca5b86a032e65cf56
Ashish Srivastav <ashishshrivastav275@gmail.com>
2022-04-22 10:17:46 +0530

    tempcommit

--- capstone-frontend/App.js
+++ capstone-frontend/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Content from "./Content";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <SideBar />
+        <Content />
+      </div>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
