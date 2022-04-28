63e7af4313a94660302fc0dbd3b2d14458931a84
Ashish Srivastav <ashishshrivastav275@gmail.com>
2022-04-28 10:17:15 +0530

    new

--- capstone-frontend/App.js
+++ capstone-frontend/App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("Pooja");
+  const [age, setAge] = useState("30");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter Name:</Text>
+      <TextInput
+        multiline
+        style={style.input}
+        placeholder="e.g.Mrie"
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter Age</Text>
+      <TextInput
+        style={style.input}
+        keyboardType="numeric"
+        placeholder="e.g.99"
+        onChangeText={(val) => setAge(val)}
+      />
+
+      <Text>
+        {" "}
+        name: {name},age: {age}{" "}
+      </Text>
     </View>
   );
 }
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
